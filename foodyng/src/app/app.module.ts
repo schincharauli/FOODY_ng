692d8f3b6626ccec25d52ffcb0a13beb08454bd8
schincharauli <52008243+schincharauli@users.noreply.github.com>
2021-04-28 15:42:39 +0400

    second step

--- foodyng/src/app/app.module.ts
+++ foodyng/src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { SliderComponent } from './slider/slider.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FeaturesComponent } from './features/features.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavigationComponent,
+    SliderComponent,
+    FeaturesComponent,
+    
+
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ foodyng/src/app/slider/slider.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap'
+
+@Component({
+  selector: 'app-slider',
+  templateUrl: './slider.component.html',
+  styleUrls: ['./slider.component.scss'],
+  providers: [NgbCarouselConfig]  // add NgbCarouselConfig to the component providers
+})
+export class SliderComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  constructor() {}
+
+  images = [ '../../assets/mogshivda.png', '../../assets/modzebne-1.png', '../../assets/last-.png', '../../assets/mogitant.png',]
+}
